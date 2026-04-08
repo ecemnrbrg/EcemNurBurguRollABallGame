2a3f48a932f94923b4c73d22c44677132d27a43b
Ecem <ecemnurburgu52@gmail.com>
2026-04-08 10:25:23 +0300

    lastpush

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -94,7 +94,7 @@ public class PlayerController : MonoBehaviour
         countText.text = "Count: " + count.ToString();
 
         // Check if the count has reached or exceeded the win condition.
-        if (count >= 12)
+        if (count >= 10)
         {
             // Display the win text.
             winTextObject.SetActive(true);
